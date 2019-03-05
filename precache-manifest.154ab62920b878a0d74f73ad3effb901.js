4cdfcaa57e14025d414667f6a68f9d242dec2640
RaquelGlez <gonzalez.c.raquel@gmail.com>
2019-03-05 17:39:32 -0600

    Updates

---
+++ precache-manifest.154ab62920b878a0d74f73ad3effb901.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "cb3305415ca0bc1ad768",
+    "url": "/static/js/main.cb330541.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "cb3305415ca0bc1ad768",
+    "url": "/static/css/main.b784fc12.chunk.css"
+  },
+  {
+    "revision": "280b6eec1f39294d1f3d61e0dc95bb95",
+    "url": "/index.html"
+  }
+];
